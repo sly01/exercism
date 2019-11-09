6e16ab873fea2a87b2a4f5c7716b871b9ee80ca1
sly01 <aerkoc01@gmail.com>
2019-11-09 12:14:16 +0100

    Improved by mentor suggestion

--- bash/error-handling/error_handling.sh
+++ bash/error-handling/error_handling.sh
@@ -1,34 +1,8 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
-
 main () {
-    if [ $# -ne 1 ]
-    then
-        echo "Usage: ./error_handling <greetee>"
-        exit 1
-    fi
+
+    (( $# != 1 )) && echo "Usage: ./error_handling <greetee>" && exit 1
 
     echo "Hello, ${1}"
 
