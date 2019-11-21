c7c22d51667e4bf2dba630aa6c4fa969754e8159
sly01 <aerkoc01@gmail.com>
2019-11-21 10:03:17 +0100

    Added v3 scrabble-score

--- python/scrabble-score/scrabble_score.py
+++ python/scrabble-score/scrabble_score.py
@@ -1,9 +1,3 @@
 def score(word):
     score_list = {"AEIOULNRST":1 ,"DG":2 ,"BCMP":3 ,"FHVWY":4 ,"K":5 ,"JX":8 ,"QZ":10}
-    keys = list(score_list.keys())
-    count = 0
-    for w in word.upper():
-        for key in keys:
-            if w in key:
-                count += score_list[key] 
-    return count
+    return sum([score_list[key] for w in word.upper() for key in score_list.keys() if w in key])
